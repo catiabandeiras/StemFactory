e1b7a6a1c106328032d94d47410956aa636a8cb7
ajscarvalho <alexandre.a.carvalho@co.sapo.pt>
2016-10-06 23:01:43 +0100

    added results and simulation events - untested

--- webserver/levelconfig/1-start.py
+++ webserver/levelconfig/1-start.py
@@ -171,4 +171,3 @@ config['level'] = {
     'shopping': ['cellA', 'cellB']
 }
 
-
--- webserver/lib/simulation/params.py
+++ webserver/lib/simulation/params.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 
+from lib.simulation.results import SimulationResults
+
+
 class FakeText(object):
     def __init__(self): self.val = ''
     def insert(self, f, t):
@@ -8,6 +11,7 @@ class FakeText(object):
         print ("\nFake Text: {}: {}".format(f, t) )
 
 
+#actually more of a broker...
 class SimulationParams(object):
 
     def __init__(self):
@@ -77,6 +81,13 @@ class SimulationParams(object):
 
         #fake text
         self.text = FakeText()
+
+        #simulation results
+        self.results = SimulationResults()
+
+
+    def example_set_simulation_output_x(self, value):
+        self.value = value
 
 
     def update(self, source):
