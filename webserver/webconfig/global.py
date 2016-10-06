505b3469f9df9a94f351c5e5cba0099170e59de9
Cátia Bandeiras <Ctia Bandeiras>
2016-10-06 15:22:37 +0100

    clear commits

--- webserver/webconfig/global.py
+++ webserver/webconfig/global.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
 config = {
-    "server.socket_port": 8080,
+    "server.socket_port": 8099,
     "tools.secureheaders.on": True
 }
+
+# cultures:
+# [
+# {
+# 	name:,
+# 	growth: [0.2, 0.19, 0.18]
+# }]
