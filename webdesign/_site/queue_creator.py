9c7d28b04058e9287a73b7c75dbe546a473e07d5
Treza <trezalves@gmail.com>
2016-10-07 11:04:27 +0100

    front-end update
    preparing front-end to be web served by github

--- webdesign/_site/queue_creator.py
+++
@@ -1,9 +0,0 @@
-import simpy
-
-def create_queue_list(env,num_resources,resource_capacity):
-    """This function creates a list with two different resources,
-    each with a given capacity for expansion technologies"""
-
-    list_of_resources = [simpy.Resource(env,resource_capacity) for res in range(num_resources)]
-        
-    return list_of_resources
--- webdesign/_site/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(
-	app=['msc_gui_new.py'],
-	setup_requires = ['py2app'],
-	)
